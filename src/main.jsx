1126652512bc5907c0801a7ee9576d0e857b1a15
Ohm <ohm22496@gmail.com>
2023-04-14 13:58:30 +0700

    config err netilify

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { Theme } from "./theme/theme";
+import { Theme } from "./theme/Theme"
+
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
